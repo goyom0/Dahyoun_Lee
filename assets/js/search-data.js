bba7d0fca4ead987d2ce66f04f1c26fdabefc225
goyom0 <163495542+goyom0@users.noreply.github.com>
2026-05-20 07:55:15 +0000

    Deploying to gh-pages from @ goyom0/Dahyoun_Lee@1f1eda6ce74ac00415cb6dcb4802ea9eb254cd04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Dahyoun_Lee/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/Dahyoun_Lee/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/Dahyoun_Lee/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-submenus",
+          title: "submenus",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Dahyoun_Lee/_pages/dropdown.html";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
