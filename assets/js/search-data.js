13a01f41ef676ce8cbe39b22624eff951b24d143
goyom0 <163495542+goyom0@users.noreply.github.com>
2026-05-20 08:06:10 +0000

    Deploying to gh-pages from @ goyom0/Dahyoun_Lee@7694dd8e4ed8a0d2400cc27ca55b9f6cdf4abe92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,21 +454,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/Dahyoun_Lee/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/Dahyoun_Lee/assets/pdf/cv_dahyoun.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%67%6F%79%6F%6D@%79%6F%6E%73%65%69.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-rss',
@@ -482,14 +475,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Y3DfzdkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
